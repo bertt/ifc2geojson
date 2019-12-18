5e8771ce2869bca09f931850edeeba4d4ccbded8
Bert Temme <btemme@hotmail.com>
2019-12-18 14:20:29 +0100

    add length units (meters or millimeter)

--- src/ifc2geojson/Ifc2GeoJSON.cs
+++ src/ifc2geojson/Ifc2GeoJSON.cs
@@ -20,61 +20,62 @@ namespace ifc2geojson
         public static void Convert(IfcStore model)
         {
             var ifcProject = model.FederatedInstances.OfType<IfcProject>().FirstOrDefault();
+            var lengthUnitPower = ifcProject.UnitsInContext.LengthUnitPower; //METRE or MILLIMETRE
             var buildings = ifcProject.Buildings;
             Console.WriteLine("Buildings: " + ifcProject.Buildings.Count());
-            HandleBuildings(ifcProject, buildings);
+            HandleBuildings(ifcProject, buildings, lengthUnitPower);
         }
 
-        private static void HandleBuildings(IfcProject ifcProject, IEnumerable<IIfcBuilding> buildings)
+        private static void HandleBuildings(IfcProject ifcProject, IEnumerable<IIfcBuilding> buildings, double lengthUnitPower)
         {
             foreach (var building in buildings)
             {
                 // Console.WriteLine("description: " + building.BuildingAddress.Description);
                 var storeys = building.BuildingStoreys;
                 var site = ifcProject.Sites.FirstOrDefault();
-                HandleStoreys(storeys, site.RefLongitude.Value.AsDouble, site.RefLatitude.Value.AsDouble);
+                HandleStoreys(storeys, site.RefLongitude.Value.AsDouble, site.RefLatitude.Value.AsDouble, lengthUnitPower);
             }
         }
 
-        private static void HandleStoreys(IEnumerable<IIfcBuildingStorey> storeys, double longitude, double latitude)
+        private static void HandleStoreys(IEnumerable<IIfcBuildingStorey> storeys, double longitude, double latitude, double lengthUnitPower)
         {
             foreach (var storey in storeys)
             {
                 Console.WriteLine("Processing storey " + storey.LongName);
-                HandleSpaces(storey.Spaces, storey.Name, longitude, latitude);
+                HandleSpaces(storey.Spaces, storey.Name, longitude, latitude, lengthUnitPower);
             }
         }
 
-        private static void HandleSpaces(IEnumerable<IIfcSpace> spaces, string storey_name, double longitude, double latitude)
+        private static void HandleSpaces(IEnumerable<IIfcSpace> spaces, string storey_name, double longitude, double latitude, double lengthUnitPower)
         {
             var storey_data = new FeatureCollection();
 
             foreach (var space in spaces)
             {
-                HandleSpace(space, storey_data, longitude, latitude);
+                HandleSpace(space, storey_data, longitude, latitude, lengthUnitPower);
             }
 
             var serializedData = JsonConvert.SerializeObject(storey_data);
             File.WriteAllText($"{storey_name}.geojson", serializedData);
         }
 
-        private static void HandleSpace(IIfcSpace space, FeatureCollection features, double longitude, double latitude)
+        private static void HandleSpace(IIfcSpace space, FeatureCollection features, double longitude, double latitude, double lengthUnitPower)
         {
             var representation = space.Representation.Representations[0].Items[0];
             if (representation is IfcExtrudedAreaSolid)
             {
-                HandleExtrudedAreaSolid((IfcExtrudedAreaSolid)representation, features, longitude, latitude);
+                HandleExtrudedAreaSolid((IfcExtrudedAreaSolid)representation, features, longitude, latitude, lengthUnitPower);
             }
             else if(representation is IfcFacetedBrep)
             {
-                HandleFacetedBrep((IfcFacetedBrep)representation, features, longitude, latitude);
+                HandleFacetedBrep((IfcFacetedBrep)representation, features, longitude, latitude, lengthUnitPower);
             }
             // todo: handle IfcPolyline?
             // todo: handle IfcBooleanClippingResult?
 
         }
 
-        private static void HandleFacetedBrep(IfcFacetedBrep facetedBrep, FeatureCollection features, double longitude, double latitude)
+        private static void HandleFacetedBrep(IfcFacetedBrep facetedBrep, FeatureCollection features, double longitude, double latitude, double lengthUnitPower)
         {
             var outer = facetedBrep.Outer;
 
@@ -89,7 +90,7 @@ namespace ifc2geojson
 
                     foreach (var pnt in polygon)
                     {
-                        var newp = AddDelta(longitude, latitude, pnt.X / 1000, pnt.Y / 1000);
+                        var newp = AddDelta(longitude, latitude, pnt.X * lengthUnitPower, pnt.Y * lengthUnitPower);
                         points.Add(new Position(newp.y, newp.x));
 
                     }
@@ -101,10 +102,9 @@ namespace ifc2geojson
 
                 }
             }
-
         }
 
-        private static void HandleExtrudedAreaSolid(IfcExtrudedAreaSolid extrudedAreaSolid, FeatureCollection features, double longitude, double latitude)
+        private static void HandleExtrudedAreaSolid(IfcExtrudedAreaSolid extrudedAreaSolid, FeatureCollection features, double longitude, double latitude, double lengthUnitPower)
         {
             var depth = extrudedAreaSolid.Depth;
 
@@ -119,7 +119,7 @@ namespace ifc2geojson
                     var points = new List<IPosition>();
                     foreach (var pnt in line)
                     {
-                        var newp = AddDelta(longitude, latitude, pnt.X / 1000, pnt.Y / 1000);
+                        var newp = AddDelta(longitude, latitude, pnt.X * lengthUnitPower, pnt.Y *lengthUnitPower);
                         points.Add(new Position(newp.y, newp.x));
                     }
                     var featureProperties = new Dictionary<string, object> { };
